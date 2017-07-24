59d5c417e0a3ba97572e73b57902ce0d282b276e
Hector Chou <hectorchou@hotmail.com>
2017-07-24 18:04:19 +0800

    install telegraf+influxdb+grafana

---
+++ install_tel_influx_grafana.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# the script for the install telegraf+influxdb+grafana for monitor the system ,
+#network and so on
+echo -e "=========================================="
+echo -e "=****************************************="
+ehco -e "====the Monitor System Install Scripts===="
+echo -e "=****************************************="
+echo -e "=========================================="
+echo "^$"
+ehco "^$"
+echo "==Author:hectorchou=="
+echo "==E-mail:hectorchou@hotmail.com=="
+echo "==Github:https://github.com/hectorchou/ShellScripts=="
+echo "==Blog:http://hectorchou.com=="
+echo "The installization is beginning..."
+
+
+
+
+ehco "The Scripts completed successfully!"
+echo "Congratulations to you. "
